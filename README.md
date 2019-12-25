b82424dfe30d299ae2773f88fc7f9a9765f33cac
dongoa <ao.dong@siat.ac.cn>
2019-12-25 17:16:50 +0800

    add extensions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ##### 天气查询案例
-API:使用高德api
-语言：typescript
-库：axios colors commander
++ API:使用高德api
++ 语言：typescript
++ 库：axios colors commander TSLint
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,80 @@
-import axios,{ AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import colors from "colors";
 import commander from "commander";
-const command=commander
-.version("0.1.0")
-.option("-c, --city [name]","Add city name")
-.parse(process.argv);
-
-if(!command.city){
+import { LOADIPHLPAPI } from "dns";
+const command = commander
+    .version("0.1.0")
+    .option("-c, --city [name]", "Add city name")
+    .option("-E, --extensions [name]", "choose weather now: [base] or future:[all]")
+    .parse(process.argv);
+// console.log(command);
+if (!command.city) {
     command.outputHelp();
 }
 
-if(process.argv.slice(2).length === 0){
+if (process.argv.slice(2).length === 0) {
     command.outputHelp(colors.red);
     process.exit();
 }
-// console.log(commander.city);
+    // console.log(commander.city);
 
-interface IWeatherResponse{
-    status:string;
-    count:string;
-    info:string;
-    infocode:string;
+interface IWeatherResponse {
+    status: string;
+    count: string;
+    info: string;
+    infocode: string;
     lives: ILive[];
 }
 
 interface ILive {
-    province:string;
-    city:string;
-    adcode:string;
-    weather:string;
-    temperature:string;
-    winddirection:string;
-    windpower:string;
-    humidity:string;
-    reporttime:string;
+    province: string;
+    city: string;
+    adcode: string;
+    weather: string;
+    temperature: string;
+    winddirection: string;
+    windpower: string;
+    humidity: string;
+    reporttime: string;
 }
 
-const URL="http://restapi.amap.com/v3/weather/weatherInfo";
+const URL = "http://restapi.amap.com/v3/weather/weatherInfo";
 const KEY = "3da410545e8ffe2c5174f48ecf719d3d";
 
+const log = console.log;
+    // axios.get(`${URL}?city=${encodeURI(command.city)}&key=${KEY}`).then((res:AxiosResponse<IWeatherResponse>)=>{
+    //     // console.log(res.data.lives);
+    //     const live=res.data.lives[0];
+    //     log(colors.yellow(live.reporttime));
+    //     log(colors.white(`${live.province} ${live.city}`));
+    //     log(colors.green(`${live.weather} ${live.temperature} 度`));
+    // }).catch(()=>{
+    //     log(colors.red("天氣服務出現異常！"));
+    // })
 
-const log=console.log;
-// axios.get(`${URL}?city=${encodeURI(command.city)}&key=${KEY}`).then((res:AxiosResponse<IWeatherResponse>)=>{
-//     // console.log(res.data.lives);
-//     const live=res.data.lives[0];
-//     log(colors.yellow(live.reporttime));
-//     log(colors.white(`${live.province} ${live.city}`));
-//     log(colors.green(`${live.weather} ${live.temperature} 度`));
-// }).catch(()=>{
-//     log(colors.red("天氣服務出現異常！"));
-// })
+    // 使用await async
+async function getWeather(city: string) {
+    try {
+        const url = `${URL}?city=${encodeURI(command.city)}&key=${KEY}&extensions=${command.extensions}`;
+        const response = await axios.get(url);
 
-//使用await async
-async function getWeather(city:string){
-    try{
-        const url=`${URL}?city=${encodeURI(command.city)}&key=${KEY}`;
-        const response=await axios.get(url);
-        const live=response.data.lives[0];
-        log(colors.yellow(live.reporttime));
-    log(colors.white(`${live.province} ${live.city}`));
-    log(colors.green(`${live.weather} ${live.temperature} 度`));
-    }catch{
+        if (response.data.forecasts) {
+            const live = response.data.forecasts;
+            log(JSON.stringify(live));
+        } else {
+            const live = response.data.lives[0];
+            log(colors.yellow(live.reporttime));
+            log(colors.white(`${live.province} ${live.city}`));
+            log(colors.green(`${live.weather} ${live.temperature} 度`));
+        }
+
+        console.log("完成查询！再次输入：");
+        command.parse(process.argv);
+    } catch {
         log(colors.red("天氣服務出現異常！"));
     }
 }
+
 getWeather(command.city);
+
+// console.log(command);
